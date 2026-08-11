8aecf916162bd84fb082f9f44d55baa6eb511ee8
ISS Round-2 <roziev0001@gmail.com>
2026-08-12 01:02:44 +0500

    chore: SSR DOM-inspection harness for visual verification

---
+++ scripts/ssr-check.tsx
@@ -0,0 +1,39 @@
+// @ts-nocheck
+import { renderToStaticMarkup } from "react-dom/server";
+import { MemoryRouter } from "react-router-dom";
+import { I18nextProvider } from "react-i18next";
+import i18n from "../src/i18n";
+import { Navbar } from "../src/widgets/navbar/nav";
+import { Footer } from "../src/widgets/footer/footer";
+import { HeaderContent } from "../src/components/header/headerContant/headerContent";
+import { Home } from "../src/pages/home/home";
+import { About } from "../src/pages/about/about";
+import { Gallery } from "../src/pages/gallery/gallery";
+
+function render(node: React.ReactElement, route = "/") {
+  return renderToStaticMarkup(
+    <I18nextProvider i18n={i18n}>
+      <MemoryRouter initialEntries={[route]}>{node}</MemoryRouter>
+    </I18nextProvider>
+  );
+}
+
+async function main() {
+  const arg = process.argv[2] || "nav";
+  const lang = process.argv[3] || "en";
+  const route = process.argv[4] || "/";
+  await i18n.changeLanguage(lang);
+
+  const map: Record<string, React.ReactElement> = {
+    nav: <Navbar />,
+    footer: <Footer />,
+    hero: <HeaderContent />,
+    home: <Home />,
+    about: <About />,
+    gallery: <Gallery />,
+  };
+  const html = render(map[arg] ?? <Navbar />, route);
+  console.log(html.replace(/></g, ">\n<"));
+}
+
+main();
